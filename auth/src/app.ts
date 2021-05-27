b556aff66daadd013a8210549315671e3ef840f8
cultureweb <cedric.milius@yahoo.fr>
2021-05-27 10:16:46 +0200

    testing signup & changing NODE_ENV during test

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -16,7 +16,10 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: true,
+    // Whenever Jest run the test, it sets variable .NODE_ENV === "test"
+    // The code below is saying If it'is equal to test set secure false
+    // Otherwise for any other kind of environment set secure to true
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
