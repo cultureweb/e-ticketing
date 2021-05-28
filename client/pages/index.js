52599f3e6af9f04e33ac7dd4f7f70b7322e83185
cultureweb <cedric.milius@yahoo.fr>
2021-05-29 00:31:15 +0200

    handling login with server side rendering

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,21 @@
-const home = () => {
-  return <h1>Landing Page</h1>;
+import buildClient from "../api/build-client";
+
+const home = ({ currentUser }) => {
+  console.log("currentUser", currentUser);
+
+  return currentUser ? (
+    <h1>you are signed in</h1>
+  ) : (
+    <h1>you are not signed in</h1>
+  );
 };
+
+home.getInitialProps = async (context) => {
+  const client = buildClient(context);
+
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
+};
+
 export default home;
