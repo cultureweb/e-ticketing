40da7a7e44c7c502f0d800d79c286e7439857e6c
cultureweb <cedric.milius@yahoo.fr>
2021-06-04 11:15:50 +0200

    Defining the Ticket Model

---
+++ tickets/src/models/ticket.ts
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+// interface describe attributes to create a ticket
+interface TicketAttrs {
+  title: string;
+  price: string;
+  userId: string;
+}
+
+// interface describe a Ticket when created
+interface TicketDoc extends mongoose.Document {
+  title: number;
+  price: number;
+  userId: string;
+}
+
+interface TicketModel extends mongoose.Model<TicketDoc> {
+  build(attrs: TicketAttrs): TicketDoc;
+}
+
+const ticketSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  toJSON: {
+    transform(doc, ret) {
+      ret.id = ret._id;
+      delete ret._id;
+    },
+  },
+});
+
+ticketSchema.statics.build = (attrs: TicketAttrs) => {
+  return new Ticket(attrs);
+};
+const Ticket = mongoose.model<TicketDoc, TicketModel>("Ticket", ticketSchema);
+
+export { Ticket };
