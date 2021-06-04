9b7b2a00e5b6bb05e01c112591311faba6cc360f
cultureweb <cedric.milius@yahoo.fr>
2021-06-04 15:45:43 +0200

    list all ticket as index route & test passed

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes/index";
 
 import { errorHandler, NotFoundError, currentUser } from "@eticketing/common";
 
@@ -22,6 +23,7 @@ app.use(
 app.use(currentUser);
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFoundError();
