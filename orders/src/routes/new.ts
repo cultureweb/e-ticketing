598a938e985e63d1537f0cfc266392b138030dfd
cultureweb <cedric.milius@yahoo.fr>
2021-06-12 17:03:00 +0200

    updatind listen events with version using mongo plugin

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -57,9 +57,8 @@ router.post(
     });
     await order.save();
 
-    // publish an event saying that an order was created
-    const client = natsWrapper.client;
-    new OrderCreatedPublisher(client).publish({
+    // Publish an event saying that an order was created
+    new OrderCreatedPublisher(natsWrapper.client).publish({
       id: order.id,
       status: order.status,
       userId: order.userId,
@@ -69,6 +68,7 @@ router.post(
         price: ticket.price,
       },
     });
+
     res.status(201).send(order);
   }
 );
