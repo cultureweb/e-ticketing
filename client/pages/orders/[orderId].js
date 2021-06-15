449f0ae0fc2433848cd0a32cb6e29a26136167bc
cultureweb <cedric.milius@yahoo.fr>
2021-06-16 00:16:32 +0200

    import and init StripeCheckout

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from "react";
+import StripeCheckout from "react-stripe-checkout";
+import useRequest from "../../hooks/use-request";
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0);
+
+  const { doRequest, errors } = useRequest({
+    url: "/api/payments",
+    method: "post",
+    body: {
+      orderId: order.id,
+    },
+    onSuccess: (payment) => console.log(payment),
+  });
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -20,7 +31,19 @@ const OrderShow = ({ order }) => {
     return <div> order has expired</div>;
   }
 
-  return <div>{timeLeft} seconds left until order expires</div>;
+  return (
+    <div>
+      {timeLeft} seconds left until order expires
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        //stripeKey="pk_test_51J2WMgA4IUzXHMRMhC8GRO0QfQXL1OEfxBvAKhJupKR67Im5YLqLu8OiQPF4wYyjnrUT2ilgwPbjubXdVdjI74Lm00XdjXwVjn"
+        stripeKey={process.env.NEXT_PUBLIC_STRIPE_KEY}
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  );
 };
 OrderShow.getInitialProps = async (context, client) => {
   const { orderId } = context.query;
