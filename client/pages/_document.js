7d2db08b19e406999a5e111396ad28810ef7efeb
cultureweb <cedric.milius@yahoo.fr>
2021-06-18 19:56:36 +0200

    Update Document

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,25 +1,22 @@
-import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
-// import favicon from '../data/imageExports';
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
 
-const MyDocument = () => {
-  return (
-    <Html>
-      <Head>
-        <link rel="shortcut icon" href="/static/favicon.ico" />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-};
-
-MyDocument.getInitialProps = async (context, client, currentUser) => {
-  const initialProps = await Document.getInitialProps(context);
-  return { ...initialProps };
-};
-
-export default MyDocument;
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="shortcut icon" href="/static/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
